4986335cf6be5d097174be9584bfdc3a7a057dba
TenseBeatle <55171076+TenseBeatle@users.noreply.github.com>
2024-12-17 21:29:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Official Helper for Noctem!", "🎮 Playing VALORANT"];
+const statusMessages = ["Official Helper for Noctem!", "Listening to Beatle :3"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
